feb16e37aa267f85ff0a723992f5c2a74490a09f
Dario Roman <darioroman2001@gmail.com>
2022-08-04 20:40:32 -0400

    📦 implement handlers for basic crud actions on posts.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,118 @@ extern crate diesel;
 
 pub mod schema;
 pub mod models;
+pub mod actions;
+
+use self::models::*;
+use self::actions::*;
+
 
 use dotenv::dotenv;
 use std::env;
 use diesel::prelude::*;
-use diesel::pg::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
+use actix_web::{get, post, put, delete, web, App, Error, HttpServer, Responder, HttpResponse};
 
-fn main() {
+type DBpool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[post("/echo")]
+async fn echo(payload: web::Json<Post>) -> impl Responder {
+    HttpResponse::Ok().json(payload)
+}
+
+#[get("/posts")]
+async fn list_posts_handler(db_pool: web::Data<DBpool>) -> Result<HttpResponse, Error> {
+    let conn = db_pool.get().expect("couldn't get db connection from pool");
+
+    let ps = web::block(move || list_posts(&conn))
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().json(ps))
+}
+
+#[get("/posts/{search_id}")]
+async fn get_post_handler(db_pool: web::Data<DBpool>, search_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+    let post = web::block(move || {
+        let conn = db_pool.get()?;
+        get_post(&conn, search_id.into_inner())
+    })
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    match post {
+        Some(p) => Ok(HttpResponse::Ok().json(p)),
+        None => Ok(HttpResponse::NotFound().json(JsonError{
+            message: "Post not found".to_string(),
+        })),
+    }
+}
+
+#[post("/posts")]
+async fn insert_post_handler(db_pool: web::Data<DBpool>, new_post: web::Json<Post>) -> Result<HttpResponse, Error> {
+    let conn = db_pool.get().expect("couldn't get db connection from pool");
+
+    let post = web::block(move || insert_post(&conn, &NewPost{
+        title: &new_post.title,
+        slug: &new_post.slug,
+        body: &new_post.body,
+    }))
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().json(post))
+}
+
+#[delete("/posts/{search_id}")]
+async fn delete_post_handler(db_pool: web::Data<DBpool>, search_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+    let conn = db_pool.get().expect("couldn't get db connection from pool");
+
+    let post = web::block(move || delete_post(&conn, search_id.into_inner()))
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().json(post))
+}
+
+#[put("/posts/{search_id}")]
+async fn update_post_handler(db_pool: web::Data<DBpool>, search_id: web::Path<i32>, new_post: web::Json<Post>) -> Result<HttpResponse, Error> {
+    let conn = db_pool.get().expect("couldn't get db connection from pool");
+
+    let post = web::block(move || update_post(&conn, search_id.into_inner(), &NewPost{
+        title: &new_post.title,
+        slug: &new_post.slug,
+        body: &new_post.body,
+    }))
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().json(post))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
 
-    let conn = PgConnection::establish(&db_url).expect("Error connecting to database");
-
-    use self::schema::posts::dsl::*;
-
-    let ps = posts.load::<models::Post>(&conn).expect("Error loading posts");
-    println!("Displaying {} posts", ps.len());
+    HttpServer::new(move || {
+        App::new()
+        .app_data(web::Data::new(pool.clone()))
+        .service(list_posts_handler)
+        .service(get_post_handler)
+        .service(insert_post_handler)
+        .service(delete_post_handler)
+        .service(update_post_handler)        
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,24 @@
-#[derive(Queryable)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub slug: String,
     pub body: String,
+}
+
+use super::schema::posts;
+
+#[derive(Insertable)]
+#[table_name = "posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub slug: &'a str,
+    pub body: &'a str,
+}
+
+#[derive(Serialize)]
+pub struct JsonError {
+    pub message: String,
 }
