843a97b4e31637658492b3add3ded4cfdcf7cd15
Dario Roman <darioroman2001@gmail.com>
2022-08-04 20:55:47 -0400

    🔧 change actix address to accept outside connections.

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ async fn main() -> std::io::Result<()> {
         .service(delete_post_handler)
         .service(update_post_handler)        
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
